92973a17d52f267ac8b43b91f2e9662d843024b6
zy11043 <zy11043@unnc-cslinux2.nottingham.edu.cn>
2014-12-09 15:56:16 +0800

    create structure

---
+++ GUIClient.java
@@ -0,0 +1,42 @@
+// 6511043 zy11043 Zhang Huayan
+
+
+package stock;
+
+
+public class GUIClient{
+    public static void main(String[] args){
+	
+	initialize();
+
+	while(true){
+	    if(!runing()){
+		break;
+	    }
+
+	    update();
+	}
+	
+	
+	end();
+    }
+
+
+    private void initialize(){
+
+    }
+
+    private void update(){
+
+    }
+
+    private void end(){
+
+    }
+
+    private boolean running(){
+
+    }
+
+
+}
---
+++ Reader.java
@@ -0,0 +1,23 @@
+// 6511043 zy11043 Zhang Huayan
+
+package stock;
+
+class Reader extends Runnable{
+
+
+    // variables
+    private Socket socket;
+    private InputStream is;
+    private Datapool datapool;
+
+    // setter
+    
+
+    // getter
+
+    // methods
+
+
+    
+
+}
